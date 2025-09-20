f4ae1190059949b186967b2d57cc0c350c3cae68
T-Visor <turkimbro@yahoo.com>
2025-09-20 11:43:48 -0400

    added persistence with local storage

--- frontend/stores/useNotesStore.ts
+++ frontend/stores/useNotesStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware"
 import { Note } from "@/types/index";
 
 interface NotesStore {
@@ -14,40 +15,49 @@ interface NotesStore {
   clearCurrentNote: () => void;
 }
 
-const useNotesStore = create<NotesStore>((set) => ({
-  notes: [],
-  setNotes: (newNotes: Note[]) => set({
-    notes: newNotes
+const useNotesStore = create<NotesStore>()(
+  persist((set) => ({
+    notes: [],
+    setNotes: (newNotes: Note[]) => set({
+      notes: newNotes
+    }),
+    clearAllNotes: () => set({
+      notes: [],
+      currentNote: null
+    }),
+    addNote: (newNote: Note) => set((state) => ({
+      notes: [...state.notes, newNote]
+    })),
+    deleteNote: (id: string) => set((state) => ({
+      notes: state.notes.filter((note) => note.id !== id),
+      currentNote: state.currentNote?.id === id ? null : state.currentNote
+    })),
+    updateNote: (
+      id: string,
+      updates: Partial<Note>
+    ) => set((state) => ({
+      notes: state.notes.map((note) =>
+        note.id === id ? { ...note, ...updates } : note
+      ),
+      currentNote:
+        state.currentNote?.id === id
+          ? { ...state.currentNote, ...updates }
+          : state.currentNote,
+    })),
+    currentNote: null,
+    setCurrentNote: (newNote: Note) => set({
+      currentNote: newNote
+    }),
+    clearCurrentNote: () => set({
+      currentNote: null
+    }),
   }),
-  clearAllNotes: () => set({
-    notes: []
-  }),
-  addNote: (newNote: Note) => set((state) => ({
-    notes: [...state.notes, newNote]
-  })),
-  deleteNote: (id: string) => set((state) => ({
-    notes: state.notes.filter((note) => note.id !== id),
-    currentNote: state.currentNote?.id === id ? null : state.currentNote
-  })),
-  updateNote: (
-    id: string,
-    updates: Partial<Note>
-  ) => set((state) => ({
-    notes: state.notes.map((note) =>
-      note.id === id ? { ...note, ...updates } : note
-    ),
-    currentNote:
-      state.currentNote?.id === id
-        ? { ...state.currentNote, ...updates }
-        : state.currentNote,
-  })),
-  currentNote: null,
-  setCurrentNote: (newNote: Note) => set({
-    currentNote: newNote
-  }),
-  clearCurrentNote: () => set({
-    currentNote: null
-  }),
-}));
+    {
+      name: "notes-storage",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({ notes: state.notes })
+    },
+  )
+);
 
 export default useNotesStore;
