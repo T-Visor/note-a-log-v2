1037eb73af39a883f689436293aad11c4b2b9cfc
T-Visor <turkimbro@yahoo.com>
2025-10-31 11:43:47 -0400

    theme switches dynamically now on editor content

--- frontend/components/note/note-content-area.tsx
+++ frontend/components/note/note-content-area.tsx
@@ -5,6 +5,7 @@ import "@blocknote/shadcn/style.css";
 
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/shadcn";
+import { useTheme } from "next-themes";
 
 interface NoteContentAreaProps {
   // Keeping your original props for future expansion
@@ -18,21 +19,22 @@ const NoteContentArea = ({
 }: NoteContentAreaProps) => {
   // Create the BlockNote editor instance
   const editor = useCreateBlockNote();
+  const { theme } = useTheme();
 
   return (
     <div
       className="
         flex-1
         border border-t-0 border-gray-200 dark:border-gray-800
-        bg-white dark:bg-gray-800 w-full
+        bg-gray-50 dark:bg-gray-800 w-full
         rounded-md rounded-t-none
         overflow-y-scroll px-3
       "
     >
       <BlockNoteView
         editor={editor}
-        theme="dark"
-        className="min-h-[400px] px-0"
+        theme={`${theme === "dark" ? "dark" : "light"}`}
+        className="min-h-[400px] px-0 text-lg"
       />
     </div>
   );
