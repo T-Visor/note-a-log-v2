b65035f8411f2da221eb8d20f04ed3fb40946021
T-Visor <turkimbro@yahoo.com>
2025-11-09 23:12:06 -0500

    added api key clien-side encryption

--- frontend/stores/useAPIKeyStore.ts
+++ frontend/stores/useAPIKeyStore.ts
@@ -1,23 +1,36 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware"
+import { persist, createJSONStorage, StateStorage } from "zustand/middleware"
 import localforage from "localforage";
+import CryptoJS from "crypto-js";
+
+const SECRET = "api-key-obfuscation-value";
 
 interface APIKeyStore {
   apiKey: string;
   setApiKey: (key: string) => void;
 }
 
-const localForageStorage = {
+const encryptedStorage: StateStorage = {
   getItem: async (name: string): Promise<string | null> => {
-    return (await localforage.getItem<string>(name)) ?? null;
+    const item = await localforage.getItem<string>(name);
+    if (!item) return null;
+
+    try {
+      const bytes = CryptoJS.AES.decrypt(item, SECRET);
+      return bytes.toString(CryptoJS.enc.Utf8);
+    }
+    catch {
+      return null;
+    }
   },
-  setItem: async (name: string, value: string): Promise<void> => {
-    await localforage.setItem(name, value);
+  setItem: async (name: string, value: string) => {
+    const encrypted = CryptoJS.AES.encrypt(value, SECRET).toString();
+    await localforage.setItem(name, encrypted);
   },
-  removeItem: async (name: string): Promise<void> => {
+  removeItem: async (name: string) => {
     await localforage.removeItem(name);
-  },
-};
+  }
+}
 
 const useAPIKeyStore = create<APIKeyStore>()(
   persist(
@@ -27,7 +40,7 @@ const useAPIKeyStore = create<APIKeyStore>()(
     }),
     {
       name: "api-key-storage",
-      storage: createJSONStorage(() => localForageStorage),
+      storage: createJSONStorage(() => encryptedStorage),
     }
   )
 );
