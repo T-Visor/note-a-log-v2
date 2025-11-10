8aaf3584e70db5e24c6061989ce2b6238d94b12b
T-Visor <turkimbro@yahoo.com>
2025-11-10 16:13:19 -0500

    made progress on Ollama form

--- frontend/components/sidebar/profile/ai-settings-dialog.tsx
+++ frontend/components/sidebar/profile/ai-settings-dialog.tsx
@@ -63,91 +63,148 @@ export const AISettingsDialog = ({
         className="px-10 py-12"
         onOpenAutoFocus={(event) => event.preventDefault()} // don't highlight api key field by default
       >
-        <div className="gap-2 text-center">
-          <div className="inline-flex items-center justify-center w-12 h-12 rounded-full bg-primary/10 mb-2">
-            <Sparkles className="w-6 h-6" />
-          </div>
-          <h1 className="text-2xl font-semibold tracking-tight text-foreground pb-3">
-            AI Configuration
-          </h1>
-          <div className="flex justify-center items-center">
-            <Tabs defaultValue="account">
+        <Tabs defaultValue="cloud">
+          <div className="gap-2 text-center">
+            <div className="inline-flex items-center justify-center w-12 h-12 rounded-full bg-primary/10 mb-2">
+              <Sparkles className="w-6 h-6" />
+            </div>
+            <h1 className="text-2xl font-semibold tracking-tight text-foreground pb-3">
+              AI Configuration
+            </h1>
+            <div className="flex justify-center items-center">
               <TabsList>
-                <TabsTrigger value="account">Cloud</TabsTrigger>
-                <TabsTrigger value="password">Local</TabsTrigger>
+                <TabsTrigger value="cloud">Cloud</TabsTrigger>
+                <TabsTrigger value="local">Local</TabsTrigger>
               </TabsList>
-            </Tabs>
+            </div>
           </div>
-        </div>
-        <form className="pb-2">
-          <Controller
-            name="title"
-            control={form.control}
-            render={({ field, fieldState }) => (
-              <Field className="!gap-2" data-invalid={fieldState.invalid}>
-                <FieldLabel htmlFor={field.name}>
-                  <Key className="size-4" />
-                  API Key
-                </FieldLabel>
-                <div className="relative">
-                  <Input
-                    {...field}
-                    id={field.name}
-                    aria-invalid={fieldState.invalid}
-                    type={showApiKey ? "text" : "password"}
-                    placeholder="sk-xxxxxxxxx"
-                    className="pr-10"
-                    autoComplete="off"
-                    onChange={(event) => setApiKey(event.target.value)}
-                    onKeyDown={(event) => {
-                      // Prevents toggling the button for showing/hiding api key
-                      if (event.key === "Enter") {
-                        event.preventDefault();
-                      }
-                    }}
-                    value={apiKey ?? ""}
-                  />
-                  <Button
-                    variant="ghost"
-                    className="absolute right-2 top-1/2 -translate-y-1/2"
-                    onClick={(event) => {
-                      event.preventDefault();
-                      event.stopPropagation();
-                      setShowApiKey(!showApiKey);
-                    }}
-                  >
-                    {showApiKey ? <EyeOff /> : <Eye />}
-                  </Button>
-                </div>
-                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-                <span className="text-xs text-muted-foreground">
-                  For security purposes, your API key is only saved for the current session. Subsequent sessions will require you to re-enter your API key. Note-a-log will never store your API key in its servers.
-                </span>
-              </Field>
-            )}
-          />
-        </form>
-        <div className="flex flex-col gap-2 pb-4">
-          <FieldLabel>
-            Model
-          </FieldLabel>
-          <Select onValueChange={setSelectedAIModel} defaultValue={selectedAIModel ?? ""}>
-            <SelectTrigger className="w-full">
-              <SelectValue placeholder="Select a model" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Google</SelectLabel>
-                <SelectItem value="google:gemini-2.5-flash">Gemini 2.5 Flash</SelectItem>
-                <SelectLabel>OpenAI</SelectLabel>
-                <SelectItem value="openai:gpt-5">GPT-5</SelectItem>
-                <SelectItem value="openai:gpt-5-mini">GPT-5 mini</SelectItem>
-                <SelectItem value="openai:gpt-4o">GPT-4o</SelectItem>
-                <SelectItem value="openai:gpt-4.1">GPT-4.1</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
+          <TabsContent value="cloud">
+            <form className="pb-2">
+              <Controller
+                name="title"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field className="!gap-2" data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor={field.name}>
+                      <Key className="size-4" />
+                      API Key
+                    </FieldLabel>
+                    <div className="relative">
+                      <Input
+                        {...field}
+                        id={field.name}
+                        aria-invalid={fieldState.invalid}
+                        type={showApiKey ? "text" : "password"}
+                        placeholder="sk-xxxxxxxxx"
+                        className="pr-10"
+                        autoComplete="off"
+                        onChange={(event) => setApiKey(event.target.value)}
+                        onKeyDown={(event) => {
+                          // Prevents toggling the button for showing/hiding api key
+                          if (event.key === "Enter") {
+                            event.preventDefault();
+                          }
+                        }}
+                        value={apiKey ?? ""}
+                      />
+                      <Button
+                        variant="ghost"
+                        className="absolute right-2 top-1/2 -translate-y-1/2"
+                        onClick={(event) => {
+                          event.preventDefault();
+                          event.stopPropagation();
+                          setShowApiKey(!showApiKey);
+                        }}
+                      >
+                        {showApiKey ? <EyeOff /> : <Eye />}
+                      </Button>
+                    </div>
+                    {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    <span className="text-xs text-muted-foreground">
+                      For security purposes, your API key is only saved for the current session. Subsequent sessions will require you to re-enter your API key. Note-a-log will never store your API key in its servers.
+                    </span>
+                  </Field>
+                )}
+              />
+            </form>
+            <div className="flex flex-col gap-2 pb-4">
+              <FieldLabel>
+                Model
+              </FieldLabel>
+              <Select onValueChange={setSelectedAIModel} defaultValue={selectedAIModel ?? ""}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a model" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Google</SelectLabel>
+                    <SelectItem value="google:gemini-2.5-flash">Gemini 2.5 Flash</SelectItem>
+                    <SelectLabel>OpenAI</SelectLabel>
+                    <SelectItem value="openai:gpt-5">GPT-5</SelectItem>
+                    <SelectItem value="openai:gpt-5-mini">GPT-5 mini</SelectItem>
+                    <SelectItem value="openai:gpt-4o">GPT-4o</SelectItem>
+                    <SelectItem value="openai:gpt-4.1">GPT-4.1</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </TabsContent>
+          <TabsContent value="local">
+            <form className="pb-6">
+              <Controller
+                name="title"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field className="!gap-2" data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor={field.name}>
+                      Ollama URL
+                    </FieldLabel>
+                    <div>
+                      <Input
+                        {...field}
+                        id={field.name}
+                        defaultValue="http://localhost:11434"
+                        aria-invalid={fieldState.invalid}
+                        placeholder="http://localhost:11434"
+                        autoComplete="off"
+                        onChange={(event) => setApiKey(event.target.value)}
+                        onKeyDown={(event) => {
+                          // Prevents toggling the button for showing/hiding api key
+                          if (event.key === "Enter") {
+                            event.preventDefault();
+                          }
+                        }}
+                        value={apiKey ?? ""}
+                      />
+                    </div>
+                    {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                  </Field>
+                )}
+              />
+            </form>
+            <div className="flex flex-col gap-2 pb-4">
+              <FieldLabel>
+                Model
+              </FieldLabel>
+              <Select onValueChange={setSelectedAIModel} defaultValue={selectedAIModel ?? ""}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a model" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Google</SelectLabel>
+                    <SelectItem value="google:gemini-2.5-flash">Gemini 2.5 Flash</SelectItem>
+                    <SelectLabel>OpenAI</SelectLabel>
+                    <SelectItem value="openai:gpt-5">GPT-5</SelectItem>
+                    <SelectItem value="openai:gpt-5-mini">GPT-5 mini</SelectItem>
+                    <SelectItem value="openai:gpt-4o">GPT-4o</SelectItem>
+                    <SelectItem value="openai:gpt-4.1">GPT-4.1</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </TabsContent>
+        </Tabs>
       </DialogContent>
     </Dialog>
   );
