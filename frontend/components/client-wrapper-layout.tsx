555b257e23a393cdd578c632c8f72b5d6c9006b5
T-Visor <turkimbro@yahoo.com>
2025-11-30 12:26:16 -0500

    added ability to delete from main UI instead of just sidebar

--- frontend/components/client-wrapper-layout.tsx
+++ frontend/components/client-wrapper-layout.tsx
@@ -1,13 +1,23 @@
-"use client"
+"use client";
 
 import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { Ellipsis, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import useNotesStore from "@/stores/useNotesStore";
 
 const ClientWrapperLayout = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
   const { state, isMobile } = useSidebar();
   const showTrigger = state === "expanded" || isMobile;
+  const { currentNote, clearCurrentNote, deleteNote } = useNotesStore();
 
   return (
     <>
@@ -22,12 +32,35 @@ const ClientWrapperLayout = ({
           "
         >
           <SidebarTrigger
-            className={`h-auto w-auto p-2 hover:cursor-pointer ${
-              !showTrigger ? "invisible pointer-events-none" : ""
-            }`}
+            className={`h-auto w-auto p-2 hover:cursor-pointer ${!showTrigger ? "invisible pointer-events-none" : ""
+              }`}
             aria-hidden={!showTrigger}
             tabIndex={showTrigger ? 0 : -1}
           />
+          {currentNote && (<DropdownMenu >
+            <DropdownMenuTrigger asChild>
+              <Button
+                className="p-2"
+                variant="ghost"
+              >
+                <Ellipsis className="size-5" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              side="bottom"
+              className="dark:bg-gray-900"
+            >
+              <DropdownMenuItem
+                className="flex justify-center items-center gap-2 hover:cursor-pointer"
+                onClick={(event) => {
+                  deleteNote(currentNote.id);
+                }}
+              >
+                <span>Delete</span>
+                <Trash className="size-3" />
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>)}
         </header>
         {children}
       </main>
