8f0f1c16d766e7f6e44b90d8cee19b9869cc3bb4
T-Visor <turkimbro@yahoo.com>
2025-12-18 00:46:37 -0500

    updated env to only use a connection string for postgres

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -1,14 +1,9 @@
 import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 
-const HOST = process.env.POSTGRES_URL_BASE;
-const USERNAME = process.env.POSTGRES_ADMIN_USERNAME;
-const PASSWORD = process.env.POSTGRES_ADMIN_PASSWORD;
-const DATABASE_NAME = process.env.POSTGRES_DATABASE_NAME;
-
 export const auth = betterAuth({
   database: new Pool({
-    connectionString: `postgres://${USERNAME}:${PASSWORD}@${HOST}/${DATABASE_NAME}`,
+    connectionString: process.env.POSTGRES_CONNECTION_STRING
   }),
   emailAndPassword: {
     enabled: true,
