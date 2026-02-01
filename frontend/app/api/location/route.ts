1b4e123130bf1f6af6c6f65f9513975aeb6dc946
T-Visor <turkimbro@yahoo.com>
2026-01-31 19:41:43 -0500

    added api route to query reverse geo

---
+++ frontend/app/api/location/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+
+export const POST = async (
+  request: NextRequest
+): Promise<NextResponse> => {
+  try {
+    const body = await request.json();
+    const { latitude, longitude } = body;
+
+    if (!latitude || !longitude)
+      return new NextResponse("Missing latitude or longitude", { status: 400 });
+
+    // Call Google Geolocation API
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${
+        latitude
+      },${
+        longitude
+      }&key=${
+        process.env.NEXT_PUBLIC_GOOGLE_GEOLOCATION_API_KEY
+      }`
+    );
+
+    console.log(response.data);
+
+    const getAddressPart = (
+      type: string,
+      nameType: "short_name" | "long_name"
+    ) => {
+      const component = response.data.results[0]?.address_components.find(
+        (component: any) => component.types.includes(type)
+      );
+      return component ? component[nameType] : null;
+    };
+
+    const location = [
+      getAddressPart("locality", "long_name"),
+      getAddressPart("administrative_area_level_1", "long_name"),
+    ].filter(Boolean) as string[]; // removes empty values from array
+
+    return NextResponse.json({ location })
+  }
+  catch (error) {
+    return new NextResponse("Invalid JSON body", { status: 400 });
+  }
+};
