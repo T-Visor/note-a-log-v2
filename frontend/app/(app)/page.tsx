3f5083f3f115e34c0fdbac79bb6b4bf6d6d6469a
T-Visor <turkimbro@yahoo.com>
2026-02-15 11:49:44 -0500

    fixed implementation, now working

--- frontend/app/(app)/page.tsx
+++ frontend/app/(app)/page.tsx
@@ -1,28 +1,44 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useNotesStore from "@/stores/useNotesStore";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 
 const NoteEditor = dynamic(() => import("@/components/note/note-editor"), {
   ssr: false,
 });
 
 const Home = () => {
-  const { setCurrentNoteUsingID, loadNotes } = useNotesStore();
-
-  // Load all notes once on component mount, always do this
-  // before attempting to set a note, seen in the code below.
-  useEffect(() => {
-    loadNotes();
-  }, []);
-
-  // if the id was supplied in the URL, try to set the current note using the ID
   const searchParams = useSearchParams();
   const noteID = searchParams.get("id");
-  if (noteID)
-    setCurrentNoteUsingID(noteID);
+  const router = useRouter();
+  const [hasLoadedInitialNotes, setHasLoadedInitialNotes] = useState(false);
+  const { setCurrentNoteUsingID, loadNotes, currentNote } = useNotesStore();
+
+  // We MUST await loadNotes before setting the flag to true
+  useEffect(() => {
+    const loadInitialNotes = async () => {
+      await loadNotes(); 
+      setHasLoadedInitialNotes(true);
+    };
+    loadInitialNotes();
+  }, [loadNotes]);
+
+  // 2. URL Listener
+  useEffect(() => {
+    // We only act if the notes are loaded AND there is an ID in the URL
+    if (hasLoadedInitialNotes && noteID) {
+      
+      // Only update the store if the ID is actually different
+      if (noteID !== currentNote?.id) {
+        setCurrentNoteUsingID(noteID);
+      }
+
+      // Clean the URL so the ?id= doesn't stay forever
+      router.replace("/", { scroll: false });
+    }
+  }, [noteID, hasLoadedInitialNotes, currentNote, setCurrentNoteUsingID, router]);
 
   return <NoteEditor />;
 };
