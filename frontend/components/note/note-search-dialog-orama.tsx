b75bba9697fe83ea19356ed403fa07df40ca880c
T-Visor <turkimbro@yahoo.com>
2026-03-08 16:50:33 -0400

    v1 of refactor, now need to test

--- frontend/components/note/note-search-dialog-orama.tsx
+++ frontend/components/note/note-search-dialog-orama.tsx
@@ -70,7 +70,7 @@ const NoteSearchDialog = ({
   // If there are search results,
   // reset selection to first item when filtered results change
   useEffect(() => {
-    searchHits.length > 0 
+    searchHits.length > 0
       ? setSelectedNoteID(searchHits[0].id)
       : setSelectedNoteID("");
   }, [searchHits]);
@@ -106,99 +106,52 @@ const NoteSearchDialog = ({
               <CommandEmpty>No results found.</CommandEmpty>
             ) : (
               <CommandGroup>
-                {searchHits.map((noteResult: any) => {
-                  // Find the first match for title and content
-                  const matchedTagsSet = new Set<string>();
-                  let locationMatch: FuseResultMatch | undefined;
-                  let titleMatch: FuseResultMatch | undefined;
-                  let contentMatch: FuseResultMatch | undefined;
-
-                  noteResult.matches?.forEach((match) => {
-                    if (match.key === "tags") {
-                      const tag =
-                        typeof match.value === "string"
-                          ? match.value
-                          : noteResult.item.tags?.[match.refIndex ?? -1];
-                      if (tag)
-                        matchedTagsSet.add(tag);
-                    }
-                    else if (match.key === "location") {
-                      locationMatch = match;
-                    }
-                    else if (match.key === "title" && !titleMatch) {
-                      titleMatch = match;
-                    }
-                    else if (match.key === "content" && !contentMatch) {
-                      contentMatch = match;
-                    }
-                  });
-
-                  // Get context for title and content
-                  const titleContext = titleMatch! && titleMatch.value && titleMatch.indices
-                    ? getMatchContext(titleMatch.value, titleMatch.indices)
-                    : noteResult.item.title;
-
-                  const contentContext = contentMatch! && contentMatch.value && contentMatch.indices
-                    ? getMatchContext(contentMatch.value, contentMatch.indices)
-                    : noteResult.item.content.substring(0, CHARACTER_CONTEXT_SIZE) +
-                    (noteResult.item.content.length > CHARACTER_CONTEXT_SIZE ? "..." : "");
-
-                  const locationContext = locationMatch! && locationMatch.value && locationMatch.indices
-                    ? getMatchContext(locationMatch.value, locationMatch.indices)
-                    : noteResult.item.location;
-
-                  const matchedTags = Array.from(matchedTagsSet);
-
-                  return (
-                    <CommandItem
-                      key={noteResult.item.id}
-                      value={noteResult.item.id}
-                      className="grid grid-cols-1 mx-2"
-                      onSelect={() => {
-                        setCurrentNote(noteResult.item);
-                        setOpen(false);
-                      }}
-                    >
-                      <div className="grid grid-cols-1 gap-1">
-                        <span className="line-clamp-1">
-                          <strong>
-                            {titleContext}
-                            {/*titleMatch ? getMatchingText(noteResult.item.title, titleMatch.indices) : <></>*/}
-                          </strong>
-                        </span>
-                        <span className="line-clamp-2">
-                          {contentContext}
-                          {/*contentMatch ? getMatchingText(noteResult.item.content, contentMatch.indices) : <></>*/}
-                        </span>
-                        <div className="flex flex-wrap gap-1.5">
-                          {matchedTags.map((tag) => (
-                            <span
-                              key={tag}
-                              className="
+                {searchHits.map((searchHit: any) => (
+                  <CommandItem
+                    key={searchHit.id}
+                    className="grid grid-cols-1 mx-2"
+                    onSelect={() => {
+                      setCurrentNote(searchHit.id);
+                      setOpen(false);
+                    }}
+                  >
+                    <div className="grid grid-cols-1 gap-1">
+                      <span className="line-clamp-1">
+                        <strong>
+                          {searchHit.document.title}
+                        </strong>
+                      </span>
+                      <span className="line-clamp-2">
+                        {searchHit.document.content.slice(0, CHARACTER_CONTEXT_SIZE)}
+                      </span>
+                      <div className="flex flex-wrap gap-1.5">
+                        {searchHit.document.tags.map((tag: any) => (
+                          <span
+                            key={tag}
+                            className="
                                 rounded-full border-2 
                                 px-1.5 py-0.5
                                 bg-gray-100 dark:bg-gray-800 
                                 text-xs text-gray-700 dark:text-gray-300 
                               "
-                            >
-                              #{tag}
-                            </span>
-                          ))}
-                        </div>
-                        {locationMatch && (
-                          <div className="flex items-center gap-1 mt-1">
-                            <div className="flex items-center gap-1 px-2 py-0.5 rounded-md">
-                              <MapPin className="size-3 text-red-600 dark:text-red-500" />
-                              <span className="text-[11px] font-medium text-amber-800 dark:text-amber-400">
-                                {locationContext}
-                              </span>
-                            </div>
-                          </div>
-                        )}
+                          >
+                            #{tag}
+                          </span>
+                        ))}
                       </div>
-                    </CommandItem>
-                  );
-                })}
+                      {searchHit.document.location && (
+                        <div className="flex items-center gap-1 mt-1">
+                          <div className="flex items-center gap-1 px-2 py-0.5 rounded-md">
+                            <MapPin className="size-3 text-red-600 dark:text-red-500" />
+                            <span className="text-[11px] font-medium text-amber-800 dark:text-amber-400">
+                              {searchHit.document.location}
+                            </span>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  </CommandItem>
+                ))}
               </CommandGroup>
             )}
           </CommandList>
