aa960c520a042f9f3f6b475918f5e610ea86f6ad
T-Visor <turkimbro@yahoo.com>
2026-04-22 22:34:11 -0400

    added back thin scrollbar for chrome

--- frontend/components/client-wrapper-layout.tsx
+++ frontend/components/client-wrapper-layout.tsx
@@ -306,7 +306,7 @@ const ClientWrapperLayout = ({
             </DropdownMenu>
           </div>)}
         </header>
-        <div className="w-full flex-1 min-h-0 overflow-y-auto">
+        <div className="w-full flex-1 min-h-0 overflow-y-auto scrollbar-chrome-thin">
           {children}
         </div>
       </main>
