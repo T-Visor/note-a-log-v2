55298fa97ce630256f64ae764a229dd61222319c
T-Visor <turkimbro@yahoo.com>
2026-07-22 23:22:36 -0400

    added rotation to pin

--- frontend/components/sidebar/sidebar-content-notes.tsx
+++ frontend/components/sidebar/sidebar-content-notes.tsx
@@ -203,7 +203,7 @@ const NoteRow = React.memo(({ noteID, isActive, onSelect }: NoteRowProps) => {
     >
       {sidebarNote?.favorite && (
         <div className="absolute left-1 top-1">
-          <Pin className="fill-blue-500 text-blue-500 dark:fill-blue-400 dark:text-blue-400 size-2.25" />        
+          <Pin className="rotate-35 fill-blue-500 text-blue-500 dark:fill-blue-400 dark:text-blue-400 size-2.25" />        
         </div>
       )}
       <div
