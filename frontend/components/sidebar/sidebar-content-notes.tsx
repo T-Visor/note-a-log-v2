c9582ac809eeb609b765ae9d14fd48db95dc14fd
T-Visor <turkimbro@yahoo.com>
2026-08-13 23:16:53 -0400

    changed the padding a bit

--- frontend/components/sidebar/sidebar-content-notes.tsx
+++ frontend/components/sidebar/sidebar-content-notes.tsx
@@ -202,7 +202,7 @@ const NoteRow = React.memo(({ noteID, isActive, onSelect }: NoteRowProps) => {
     `}
     >
       {sidebarNote?.favorite && (
-        <div className="absolute left-2.5 top-1">
+        <div className="absolute left-3 top-1.5">
           <Pin className="rotate-35 fill-blue-500 text-blue-500 dark:fill-blue-400 dark:text-blue-400 size-2.25" />
         </div>
       )}
